bd0f516dcd4d5568bfbd1ee234e4efe8ccbed269
andrewmeng810 <andrewmeng810@gmail.com>
2016-09-11 15:09:39 -0400

    Update Car Attributes Analysis.R

--- Car Attributes Analysis.R
+++ Car Attributes Analysis.R
@@ -1,6 +1,17 @@
-car <- read.csv("car.txt", sep="")
+
+
+car <- read.csv("~/car.txt", sep="")
+head(car)
 car1<- car[, 3:16]
 head(car1)
+
+######################################################################################################
+######  Cleaning the Data  ###########################################################################
+######################################################################################################
+
+
+# First, I label all the attributes into 14 variables
+
 x1<- car$wheel.base
 x2<- car$length
 x3<- car$width
@@ -16,17 +27,33 @@ x12<- car$city.mpg
 x13<- car$highway.mpg
 x14<- car$price
 
-# 2.1
+######################################################################################################
+###########  Descriptive Analysis  ###################################################################
+######################################################################################################
+
+# In order to find the potential relationship between 14 variables, I calculated the coveriance and correlation matrices among 14 vaiables
+
+# Converiance Matrix
 round(cov(car1),1)
+
+# Correlation Matrix
 round(cor(car1),1)
+
 
 #pairs(car)
 
-# 2.2 
-mod11<- lm(cbind(x12,x13)~x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x14)
-summary(mod11)
+######################################################################################################
+#####  Simple Linear Regression  #####################################################################
+######################################################################################################
 
-summary(manova(mod11))
+#  By combining city.mpg and highway.mpg two variables together, I am trying to apply a simple linear regression to predict the 
+#  the city.mpg and highway.mpg
+
+
+Model_1<- lm(cbind(x12,x13)~x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x14)
+summary(Model_1)
+
+summary(manova(Model_1))
 # x4, x6, x7, x8, x10, x11, and x14 are not significant in predicting the dependent variable
 
 mod12<- lm(cbind(x12,x13)~ x1+x2+x3+x5+x9)
@@ -36,12 +63,19 @@ summary(mod12)
 
 summary(manova(mod12))
 
-######   2.3 & 2.4
+######################################################################################################
+#######  Reducing Dimensions of object data using PCA Methodology ####################################
+######################################################################################################
+
+
 CAR.pca<- princomp(car1,cor  = TRUE)  
 summary(CAR.pca, loadings= TRUE)
 # By using PCA, we could use 4 principal components to reduce the dimensions, as cumulatively, four principal components could represent 87% of the variance. 
 # Based on the first principal component, it is clear that cars with smaller sizes, lighter in weights, and higher city or highway mpg could contribute to a higher PC1 component value. 
 
+######################################################################################################
+####   Data Visualization  ###########################################################################
+######################################################################################################
 
 screeplot(CAR.pca, type = "line")
 abline(h= 1,col= "red")
@@ -70,15 +104,19 @@ hc111<-hclust(CAR, "single");
 hc222<-hclust(CAR, "complete") 
 hc333<-hclust(CAR, "median"); 
 hc444<-hclust(CAR, "ward")
-opar <- par(mfrow = c(2, 2))
+
+opar <- par(mfrow = c(2, 2))    # plot four graphs in one page
+
 plot(hc111,hang=-1,labels = car[,2], main = "Clustering Using Single Method"); re1<-rect.hclust(hc111,k=3,border="red")
 plot(hc222,hang=-1,labels = car[,2], main = "Clustering Using Complete Method");re2<-rect.hclust(hc222,k=3,border="red")
 plot(hc333,hang=-1,labels = car[,2], main = "Clustering Using Median Method"); re3<-rect.hclust(hc333,k=3,border="red")
 plot(hc444,hang=-1,labels = car[,2], main = "Clustering Using Ward Method");re4<-rect.hclust(hc444,k=3,border="red")
 
 
+######################################################################################################
+#### Factor Analysis using Exploratory Factor Analysis (EFA)     #####################################
+######################################################################################################
 
-#### 2.5
 
 CAR.EFA <- factanal(car1, 3, rotation="varimax", scores="regression")
 names(CAR.EFA)
@@ -98,8 +136,6 @@ print(CAR.EFA1, digits=2, cutoff=.6, sort=TRUE)
 # Accordingly, the car dataset can be divided into three latent variables, fuel efficiency, height, and engine performance.
 
 
-
-
 CAR.EFA1$loadings
 CAR.EFA.loadings <- CAR.EFA1$loadings[,1:2] 
 plot(CAR.EFA.loadings,type="n") # set up plot 
@@ -110,6 +146,7 @@ text(CAR.EFA.loadings,labels=names(car),cex=.7) # add variable names
 CAR.EFA.scores <- CAR.EFA1$scores[,1:2] 
 plot(CAR.EFA.scores,type="n") # set up plot 
 text(CAR.EFA1$scores[,1],CAR.EFA1$scores[,2]) # add variable names
+
 # As we already come up with three factors in the previous question. Therefore, we are able to better classify the data in the car dataset.
 # The dimensions have been reduced. In the score plot, I only used the two factors’ scores in explaining the data. 
 # As the factor 1 reflects the fuel efficiency, therefore, the lowest score in factor 1 will indicate the most fuel efficiency car in the dataset, which is the 7th car, the Chevrolet. This is same result we can get from the PCA analysis for the PC1. In addition, those 33rd, 22nd, 17th cars (Porsche, Mercedes, and jaguar2) who has the higher factor 1 scores, will indicate the least fuel efficiency and correspondently have higher prices.
@@ -117,9 +154,14 @@ text(CAR.EFA1$scores[,1],CAR.EFA1$scores[,2]) # add variable names
 
 
 # Factor plot between factor 1 and factor 3
+
+
+
 CAR.EFA.scores1 <- CAR.EFA1$scores[,c(1,3)] 
 plot(CAR.EFA.scores1,type="n") # set up plot 
 text(CAR.EFA1$scores[,1],CAR.EFA1$scores[,2]) # add variable names
+
+
 
 # Factor plot between factor 2 and factor 3
 CAR.EFA.scores2 <- CAR.EFA1$scores[,c(2,3)] 
